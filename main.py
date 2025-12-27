7dd80c33b25851363e2b00c1d189435d6a0162f3
Ziptino9098 <huddmorris@gmail.com>
2025-12-27 18:02:58 -0500

    main.py update
    Added virus detection

--- main.py
+++ main.py
@@ -19,4 +19,9 @@ if password == windows_password:
 else: print("Wrong password")
 app_to_be_installed = input("What app will you install:")
 print(app_to_be_installed + " Will be installed")
+app_is_virus = False
+if app_is_virus == True:
+    print("App was blocked due to containing malware")
+else:
+    print("No threats found in app")
 
