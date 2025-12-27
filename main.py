6df45e3ecb8052bafd6c0cd8667b660fe79e47a4
Ziptino9098 <huddmorris@gmail.com>
2025-12-27 18:22:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
-
+#Edition selector
 windows_edition = input("What Windows 11 edition will you install? Options are 'home' or 'pro': ").strip().lower()
-
+#Installer
 if windows_edition == "home":
     print("You will be installing Windows 11 Home")
 else:
@@ -9,6 +9,7 @@ else:
 
 time.sleep(12)
 print("Windows 11 has been installed")
+#User creation
 windows_username = input("Enter windows username:")
 print(windows_username + " Will be your username")
 windows_password = input("Enter windows password:")
@@ -16,12 +17,33 @@ print(windows_password + " Will be your password")
 password = input("Enter password to login:")
 if password == windows_password:
     print("Logged in")
-else: print("Wrong password")
+else:
+    print("Wrong password")
 app_to_be_installed = input("What app will you install:")
 print(app_to_be_installed + " Will be installed")
-app_is_virus = False
+#Virus protection
+app_is_virus = True
+protection_reachable = True
 if app_is_virus == True:
     print("App was blocked due to containing malware")
+elif protection_reachable == False:
+    print("App was blocked as malware protection could not be reached")
 else:
     print("No threats found in app")
+
+will_scan_run = input("Do you want to run a Virus Detection System Scan? (y/n): ").strip().lower()
+if will_scan_run == "y":
+    print("Running Virus Detection Scan")
+else:
+    print("System will not be scanned")
+
+time.sleep(2)
+
+virus_detected = True
+if virus_detected == True:
+    print("Virus Detected and was removed")
+
+else:
+    print("Virus Detected and was removed")
+
 
