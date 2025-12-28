799d8a9d05a00fcb8893fd29620d6f75df109354
Ziptino9098 <huddmorris@gmail.com>
2025-12-28 12:16:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,217 +1,105 @@
-#This is a mimic of Windows 11 installer and command prompt to show a basic os coded in python
 import time
 
-# Edition selector
-windows_edition = input(
-    "What Windows 11 edition will you install? Options are 'home' or 'pro': ")
 
-# Installer
-if windows_edition == "home":
-    print("You will be installing Windows 11 Home")
-else:
-    print("You will be installing Windows 11 Pro")
-
-time.sleep(2)
-print("Windows 11 has been installed")
-
-# User creation
-windows_username = input("Enter windows username: ")
-print(windows_username + " will be your username")
-
-windows_password = input("Enter windows password: ")
-print("Password set")
-
-password = input("Enter password to login: ")
-if password == windows_password:
-    print("Logged in")
-else:
-    print("Wrong password")
-
-# App install
-app_to_be_installed = input("What app will you install: ")
-print(app_to_be_installed + " will be installed")
-
-# Virus protection
-app_is_virus = True
-protection_reachable = True
-
-if app_is_virus:
-    print("App was blocked due to containing malware")
-elif not protection_reachable:
-    print("App was blocked as malware protection could not be reached")
-else:
-    print("No threats found in app")
-
-# Scan prompt
-will_scan_run = input(
-    "Do you want to run a Virus Detection System Scan? (y/n): ")
-
-if will_scan_run == "y":
-    print("Running Virus Detection Scan")
-else:
-    print("System will not be scanned")
-
-time.sleep(2)
-
-virus_detected = True
-if virus_detected:
-    print("Virus detected and removed")
-else:
-    print("No viruses found")
-
-# System stability
+windows_username = ""
+windows_password = ""
 is_system_stable = True
 
-if is_system_stable:
-    print("System is stable")
-else:
-    print("System Crash: ERR_CRITICAL_PROCESS_DIED")
-
-# Command execution
-command = input("Enter Command: ")
-print(command + " was executed successfully")
-
-# Restart
-restart_set = input("Would you like to restart? (y/n): ")
-
-if restart_set == "y":
-    print("Rebooting...")
-    time.sleep(3)
-    print("Rebooted")
-
-login_passwd_2 = input("Enter Login Password:")
-
-if login_passwd_2 == windows_password:
-    print("Logged in")
-
-else:
-    print("Wrong password")
-
-appinstall = input("Would you like to install in app?")
-if appinstall == "y":
-    app2 = input("What app will you install?")
-    print(app2 + " will be installed")
-
-else:
-    print("app will not be installed")
-
-com2 = input("Enter Command:")
-if com2 == "reboot":
-    print("Rebooting...")
-    time.sleep(3)
-    print("Rebooted")
-else:
-    print("Command Executed Successfully")
-
-
-command3 = input("Enter Command:")
-if command3 == "reboot":
-    print("Rebooting...")
-    time.sleep(3)
-    print("Rebooted")
-elif command3 == "del":
-    file_to_be_deleted = input("What file do you want to delete?")
-    print(file_to_be_deleted + " has been deleted")
-
-elif command3 == "install":
-    input("What app do you want to install?")
-    time.sleep(3)
+def install_windows():
+    global windows_username, windows_password
+    edition = input("Windows 11 edition (home/pro): ").lower()
+    print(f"Installing Windows 11 {'Home' if edition == 'home' else 'Pro'}...")
+    time.sleep(2)
     print("Installed")
+    windows_username = input("Username: ")
+    windows_password = input("Password: ")
+    print("Password set")
 
-else:
-    print("Command Executed Successfully")
+def login():
+    password = input("Login password: ")
+    if password == windows_password:
+        print("Logged in")
+    else:
+        print("Wrong password")
 
+def install_app(app_name=None):
+    if not app_name:
+        app_name = input("App name to install: ")
+    blocked = app_name.lower() in ["noescape.exe","wannacry.exe","iloveyou.exe","quasarrat.exe","virus.exe"]
+    if blocked:
+        print("App blocked due to malware")
+    else:
+        print(f"{app_name} installed")
 
-willreboot = input("You need to restart to finish installing the app (y/n): ")
-if willreboot == "y":
+def delete_file():
+    file = input("File to delete: ")
+    print(f"{file} deleted")
+
+def delete_app():
+    app = input("App to delete: ")
+    print(f"{app} deleted")
+
+def change_directory():
+    dir = input("Directory to change to: ")
+    print(f"Current directory is now {dir}")
+
+def reboot():
     print("Rebooting...")
-    time.sleep(3)
+    time.sleep(2)
     print("Rebooted")
-else:
-    print("System will not be rebooted")
 
-#BSOD Check
-if is_system_stable == True:
-    print("System is stable")
-else:
-    print("ERR_CRITICAL_PROCESS_DIED")
-    time.sleep(3)
-    print("Rebooting...")
-    time.sleep(3)
-    print("Rebooted")
+def virus_scan():
+    print("Running virus scan...")
+    time.sleep(2)
+    print("Virus detected and removed")
 
-login_passwd_3 = input("Enter Login Password:")
-if login_passwd_3 == windows_password:
-    print("Logged in")
-else:
-    print("Wrong password")
+def app_store():
+    app = input("App store install: ")
+    install_app(app)
 
+def send_email():
+    email = input("Send email to: ")
+    if email.lower().startswith("noreply"):
+        print("Cannot email noreply addresses")
+    else:
+        print(f"Email sent to {email}")
 
+def check_status():
+    if is_system_stable:
+        print("System is stable")
+    else:
+        print("ERR_CRITICAL_PROCESS_DIED")
 
-kommand = input("Enter Command: ")
-if kommand == "reboot":
-    print("Rebooting...")
-    time.sleep(3)
-
-elif kommand == "install":
-    app67 = input("What app do you want to install?")
-    print(app67 + " will be installed")
-
-elif kommand == "del":
-    del67 = input("What file do you want to delete?")
-    print(del67 + " will be deleted")
-
-elif kommand == "cd":
-    dir = input("What dir do you want to change to?")
-    print(dir + " is now your current directory")
-
-elif kommand == "delapp":
-    delapp67 = input("What app do you want to delete?")
-    print(delapp67 + " will be deleted")
-
-else:
-    print("Command Executed Successfully")
-
-
-appstoreapp = input("Welcome to the Windows app store! We have hundreds of apps for you to install! Just type in what to install!")
-if appstoreapp == "noescape.exe":
-    print("App was blocked due to containing malware")
-
-elif appstoreapp == "wannacry.exe":
-    print("App was blocked due to containing malware")
-
-elif appstoreapp == "iloveyou.exe":
-    print("App was blocked due to containing malware")
-
-elif appstoreapp == "quasarrat.exe":
-    print("App was blocked due to containing malware")
-
-elif appstoreapp == "virus.exe":
-    print("App was blocked due to containing malware")
-
-else:
-    print(appstoreapp + " has been installed")
+def shell():
+    while True:
+        cmd = input("\nCommand (type help for command options,'exit to quit): ").lower()
+        if cmd == "help":
+            print("install, del, delapp, cd, reboot, scan, appstore, email, status, exit")
+        elif cmd == "install":
+            install_app()
+        elif cmd == "del":
+            delete_file()
+        elif cmd == "delapp":
+            delete_app()
+        elif cmd == "cd":
+            change_directory()
+        elif cmd == "reboot":
+            reboot()
+        elif cmd == "scan":
+            virus_scan()
+        elif cmd == "appstore":
+            app_store()
+        elif cmd == "email":
+            send_email()
+        elif cmd == "status":
+            check_status()
+        elif cmd == "exit":
+            print("Exiting cmd")
+            break
+        else:
+            print(f"{cmd} executed successfully")
 
 
-
-
-myemail = input("Enter your email to send and recive emails:")
-send_email_to = input("Who would you like to email?")
-if send_email_to == "noreply@gmail.com":
-    print("That email is no reply you cannot email them")
-
-elif send_email_to == "noreply@outlook.com":
-    print("That email is no reply you cannot email them")
-
-elif send_email_to == "noreply@yahoo.com":
-    print("That email is no reply you cannot email them")
-
-elif send_email_to == "noreply@icloud.com":
-    print("That email is no reply you cannot email them")
-
-else:
-    print("Email sent to "+send_email_to)
-
-
-
-
+install_windows()
+login()
+shell()
